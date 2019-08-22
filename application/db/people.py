fc1a5223eebf8b3ba698699fa253295f413498df
Marat <lmar@mail.ru>
2019-08-22 17:42:29 +0300

    decorators added

--- application/db/people.py
+++ application/db/people.py
@@ -1,6 +1,7 @@
 # people.py
 from decors import logger_askfile
 
-#@logger_askfile
-def get_employees():
+@logger_askfile
+def get_employees(*args, **kwargs):
 	print('this is a people.py')
+	return args, kwargs
--- decors.py
+++ decors.py
@@ -5,22 +5,20 @@ def logger(func):
 		func_call_datetime = datetime.datetime.now()
 		filename = 'locallogger'
 		print('log file name: ', filename)
-		func_data = func(*args, ** kwargs)
+		func_data = func(*args, **kwargs)
+		with open (filename + '.log', 'a') as file:
+			file.write(f'{func_call_datetime} We call for {func.__name__} with args {func_data}\n')
 		return func_data
-		with open (f'{filename}.log', 'a') as file:
-			file.write(f'We call for {func.__name__} at time {func_call_datetime}\n')
-			file.write(f'Args was {func_data}\n')
 	return new_func
 
 def logger_askfile(func):
 	def new_func(*args, **kwargs):
-		#filename = input('Input log file name')
-		filename = 'locallogger1'
+		filename = input('Input log file name')
+		#filename = 'locallogger1'
 		func_call_datetime = datetime.datetime.now()
 		print('log file name: ', filename)
 		func_data = func(*args, ** kwargs)
 		return func_data
-		with open (f'{filename}.log', 'a') as file:
-			file.write(f'We call for {func.__name__} at time {func_call_datetime}\n')
-			file.write(f'Args was {func_data}\n')
+		with open (filename + '.log', 'a') as file:
+			file.write(f'{func_call_datetime} We call for {func.__name__} with args {func_data} \n')
 	return new_func
